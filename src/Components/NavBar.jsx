d30129190bbf12d47b31efcfdde747286a5a418d
Santiago manuel Cei <santiagomanuelcei@MacBook-Air-de-Santiago.local>
2021-09-02 20:01:43 -0300

    segunda entrega

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import CarWidget from'./CarWidget';
+import './NavBar.css';
+
+
+function NavBar() {
+    return (
+        <div className = "Navbar">
+            
+            <nav className = "Nav">   
+                    <ul className = "NavMenu">
+                        <li><button className ="boton"><a href = "Inicio">Inicio</a></button></li>
+                        <li><button className ="boton"><a href = "Productos">Productos</a></button></li>
+                        <li><button className ="boton"><a href = "Nosotros">Nosotros</a></button></li>
+                        <li><button className ="boton"><a href = "Contacto">Contacto</a></button></li>
+                    </ul>
+       
+                
+            </nav>
+            
+           <CarWidget/>
+        </div>
+        
+    )
+    
+}
+
+export default NavBar
---
+++ src/Container/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemListContainer() {
+    return (
+        <div>
+            <h2>Bienvenido a la Tienda </h2>
+        </div>
+    )
+}
+
+export default ItemListContainer
